b9f78f7039cb627e120cbd3250e2859db6e43bd1
unknown <maretanur197@gmail.com>
2023-07-25 23:58:33 +0700

    update: reschedule history

--- user_updateReservasi.php
+++ user_updateReservasi.php
@@ -1,30 +1,21 @@
- <?php
-    include_once("db/koneksi.php");
+<?php
+include_once("db/koneksi.php");
 
-    if (isset($_POST['update_reservation'])) {
-        $reservasi_id = $_POST['reservation_id'];
-        $nama = $_POST['nama'];
-        $alamat = $_POST['alamat'];
-        $usia = $_POST['usia'];
-        $layanan = $_POST['layanan'];
-        $tanggal = $_POST['tanggal'];
-        $waktu = $_POST['waktu'];
+if (isset($_POST['update_reservation'])) {
+    $reservasi_id = $_POST['reservation_id'];
+    $nama = $_POST['nama'];
+    $alamat = $_POST['alamat'];
+    $usia = $_POST['usia'];
+    $layanan = $_POST['layanan'];
+    $tanggal = $_POST['tanggal'];
+    $waktu = $_POST['waktu'];
 
-        $query = "UPDATE reservasi SET nama='$nama', alamat='$alamat', usia='$usia', layanan='$layanan', tanggal='$tanggal', waktu='$waktu' WHERE reservasi_id='$reservasi_id'";
-        mysqli_query($conn, $query);
+    $query = "UPDATE reservasi SET nama='$nama', alamat='$alamat', usia='$usia', layanan='$layanan', tanggal='$tanggal', waktu='$waktu' WHERE reservasi_id='$reservasi_id'";
+    mysqli_query($conn, $query);
 
-        header("Location: user_history.php");
-        exit;
-    }
-    ?>
-
+    header("Location: user_history.php");
+    exit;
+}
+?>
 
  <?php include("partials/header.php"); ?>
-
-
-
-
-
- </body>
-
- </html>
